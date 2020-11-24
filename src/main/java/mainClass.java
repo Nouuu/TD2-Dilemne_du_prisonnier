b313d79eb6adeebab14b709e5ba37a3bde892343
Nouu <noelarrieulacoste@yahoo.fr>
2020-11-24 16:45:30 +0100

    new main

--- src/main/java/mainClass.java
+++ src/main/java/mainClass.java
@@ -1,6 +1,6 @@
 public class mainClass {
     public static void main(String[] args) {
-        int iteration = 4;
+        int iteration = 10;
 
         Agent agent1 = new Agent("Agent 1");
         agent1.AddVoteSystem(new SystemVote_CooperateThenBetrayAtEnd());
@@ -12,24 +12,24 @@ public class mainClass {
 
 
         for (int i = 1; i <= iteration; i++) {
-            System.out.println("Iteration " + i);
+            System.out.println("/////// Iteration " + i + " ///////\n");
             previousAgent1Answer = agent1.ComputeVote(i, previousAgent2Answer, iteration);
             previousAgent2Answer = agent2.ComputeVote(i, previousAgent1Answer, iteration);
 
             if (previousAgent1Answer == previousAgent2Answer) {
                 if (previousAgent1Answer == VoteAnswer.cooperate) {
-                    System.out.println("Both cooperate case");
+                    System.out.println("\nBoth cooperate case");
                     agent1.addReward(10f);
                     agent2.addReward(10f);
                 } else {
-                    System.out.println("Both betray case");
+                    System.out.println("\nBoth betray case");
                 }
             } else {
                 if (previousAgent1Answer == VoteAnswer.cooperate) {
-                    System.out.println("Agent 1 betrayed by Agent 2 case");
+                    System.out.println("\nAgent 1 betrayed by Agent 2 case");
                     agent2.addReward(20f);
                 } else {
-                    System.out.println("Agent 2 betrayed by Agent 1 case");
+                    System.out.println("\nAgent 2 betrayed by Agent 1 case");
                     agent1.addReward(20f);
                 }
             }
